097203eced9548255e78a85dbd477a3ee5ab4033
Emerick94 <lucas.emerick94@gmail.com>
2020-06-18 02:56:30 -0300

    add initial functions and fix test

--- lib/plateau_mapper.ex
+++ lib/plateau_mapper.ex
@@ -1,3 +1,18 @@
 defmodule MarsProbes.PlateauMapper do
+  use Agent
 
+  @doc """
+  Starts a new plateau.
+  """
+  def start_link(x, y) do
+    Agent.start_link(fn ->
+      %{
+        size: %{x: x, y: y}
+      }
+    end)
+  end
+
+  def get(plateau, key) do
+    Agent.get(plateau, &Map.get(&1, key))
+  end
 end
--- test/plateau_mapper_test.exs
+++ test/plateau_mapper_test.exs
@@ -3,7 +3,7 @@ defmodule MarsProbes.PlateauMapperTest do
   doctest MarsProbes
 
   test "stores plateau size" do
-    {:ok, plateau} = MarsProbes.PlateauMapper.start_link(4,7)
-    assert MarsProbes.PlateauMapper.get(plateau, "size") == %{x: 4, y: 7}
+    {:ok, plateau} = MarsProbes.PlateauMapper.start_link(4, 7)
+    assert MarsProbes.PlateauMapper.get(plateau, :size) == %{x: 4, y: 7}
   end
 end
