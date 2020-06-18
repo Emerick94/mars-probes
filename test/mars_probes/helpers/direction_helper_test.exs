c588f4c64acbefadd2a32fb52d130c8ae6c7b5c1
Emerick94 <lucas.emerick94@gmail.com>
2020-06-18 17:24:40 -0300

    add tests for direction()

--- test/mars_probes/helpers/direction_helper_test.exs
+++ test/mars_probes/helpers/direction_helper_test.exs
@@ -7,6 +7,16 @@ defmodule MarsProbes.DirectionHelperTest do
     assert direction == "N"
   end
 
+  test "get direction from float with decimal part, rouding down as needed" do
+    direction = MarsProbes.DirectionHelper.direction(0.2)
+    assert direction == "N"
+  end
+
+  test "get direction from float with decimal part, rouding up as needed" do
+    direction = MarsProbes.DirectionHelper.direction(0.8)
+    assert direction == "E"
+  end
+
   test "get direction from character lower case" do
     direction = MarsProbes.DirectionHelper.direction("e")
     assert direction == "E"
@@ -20,5 +30,15 @@ defmodule MarsProbes.DirectionHelperTest do
   test "get direction from string regardeless of case" do
     direction = MarsProbes.DirectionHelper.direction("WeSt")
     assert direction == "W"
+  end
+
+  test "retur error geting direction from invalid string" do
+    direction = MarsProbes.DirectionHelper.direction("westeros")
+    assert direction == {:error, "no direction related to this string"}
+  end
+
+  test "retur error geting direction from invalid string who is a substring of a valid one" do
+    direction = MarsProbes.DirectionHelper.direction("no")
+    assert direction == {:error, "no direction related to this string"}
   end
 end
