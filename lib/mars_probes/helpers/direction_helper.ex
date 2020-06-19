8854ca562899d04e50ba158482a8d0f88145558f
Emerick94 <lucas.emerick94@gmail.com>
2020-06-19 01:11:49 -0300

    shift focus to the way i am storing the values, add direction_number

--- lib/mars_probes/helpers/direction_helper.ex
+++ lib/mars_probes/helpers/direction_helper.ex
@@ -1,26 +1,46 @@
 defmodule MarsProbes.DirectionHelper do
   @valid_names ["N", "E", "S", "W", "NORTH", "EAST", "SOUTH", "WEST"]
   @directions "NESW"
-  @direction_number %{0 => "N", 1 => "E", 2 => "S", 3 => "W"}
 
-  def direction_name(number) when is_integer(number) do
-    direction_name = rem(number, 4)
-    Map.get(@direction_number, direction_name)
+  def direction_name(direction) do
+    case direction_number(direction) do
+      number when is_integer(number) ->
+        String.at(@directions, number)
+
+      {:error, error_mesage} ->
+        {:error, error_mesage}
+    end
   end
 
-  def direction_name(number) when is_float(number) do
+  def direction_number(number) when is_integer(number) do
+    rem(number, 4)
+  end
+
+  def direction_number(number) when is_float(number) do
     number
     |> round()
-    |> direction_name()
+    |> direction_number()
   end
 
-  def direction_name(name) when is_bitstring(name) do
+  def direction_number(name) when is_bitstring(name) do
     normalized_name = String.upcase(name)
 
     if Enum.member?(@valid_names, normalized_name) do
-      String.at(normalized_name, 0)
+      letter = String.at(normalized_name, 0)
+      {number, 1} = :binary.match(@directions, letter)
+      number
     else
-      {:error, "no direction related to this string"}
+      {:error, "no direction related to this string."}
     end
+  end
+
+  def direction_number(name) when is_atom(name) do
+    name
+    |> Atom.to_string()
+    |> direction_number()
+  end
+
+  def direction_number(_invalid) do
+    {:error, "this argument is invalid, please use integer, float, string or atom."}
   end
 end
