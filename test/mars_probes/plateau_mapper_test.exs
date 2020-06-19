f27284f912ca29a5d76c8455b8a1fed470c0e2c0
Emerick94 <lucas.emerick94@gmail.com>
2020-06-19 14:23:05 -0300

    add tests for movement

--- test/mars_probes/plateau_mapper_test.exs
+++ test/mars_probes/plateau_mapper_test.exs
@@ -1,13 +1,18 @@
 defmodule MarsProbes.PlateauMapperTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
   alias MarsProbes.PlateauMapper
   doctest PlateauMapper
 
   setup do
     plateau = PlateauMapper.start_plateau(5, 6)
+    plateau = PlateauMapper.add_probe(plateau, 1, 2, "N")
+
     %{plateau: plateau}
   end
 
+  # start_plateau
   test "stores plateau initial state" do
     plateau = PlateauMapper.start_plateau(4, 7)
 
@@ -17,13 +22,71 @@ defmodule MarsProbes.PlateauMapperTest do
            }
   end
 
-  test "deploy probe", %{plateau: plateau} do
-    plateau = PlateauMapper.add_probe(plateau, 1, 2, "N")
-    assert plateau.probes == [%{x: 1, y: 2, direction: 0}]
+  # add_probe
+  test "add valid probe", %{plateau: plateau} do
+    plateau = PlateauMapper.add_probe(plateau, 2, 1, "E")
+    assert plateau.probes == [%{direction: 1, x: 2, y: 1}, %{direction: 0, x: 1, y: 2}]
   end
 
-  test "show probes", %{plateau: plateau} do
-    plateau = PlateauMapper.add_probe(plateau, 1, 2, "N")
-    assert PlateauMapper.probes() == [%{x: 1, y: 2, direction: "N"}]
+  test "plateu unchanged when adding invalid probe", %{plateau: plateau} do
+    plateau = PlateauMapper.add_probe(plateau, 7, 6, "N")
+    assert plateau.probes == [%{direction: 0, x: 1, y: 2}]
+  end
+
+  # test "plateu unchanged when adding invalid probe", %{plateau: plateau} do
+  #   execute_add_probe = fn ->
+  #     PlateauMapper.add_probe(plateau, 7, 6, "N")
+  #   end
+
+  #   assert capture_io(execute_add_probe) == "x position is invalid"
+  # end
+
+  test "add multiple valid probes", %{plateau: plateau} do
+    plateau = PlateauMapper.add_probe(plateau, 2, 4, "W")
+    plateau = PlateauMapper.add_probe(plateau, 4, 5, "S")
+
+    assert plateau.probes == [
+             %{direction: 2, x: 4, y: 5},
+             %{direction: 3, x: 2, y: 4},
+             %{direction: 0, x: 1, y: 2}
+           ]
+  end
+
+  # show_probes
+  test "show a probe", %{plateau: plateau} do
+    assert PlateauMapper.show_probes(plateau) == ["1 2 N"]
+  end
+
+  test "show two probes", %{plateau: plateau} do
+    plateau = PlateauMapper.add_probe(plateau, 3, 1, "E")
+    assert PlateauMapper.show_probes(plateau) == ["1 2 N", "3 1 E"]
+  end
+
+  # single_instruction
+  test "turn drone to the left", %{plateau: plateau} do
+    plateau = PlateauMapper.single_instruction(plateau, "L")
+    assert PlateauMapper.show_probes(plateau) == ["1 2 W"]
+  end
+
+  test "turn drone to the right", %{plateau: plateau} do
+    plateau = PlateauMapper.single_instruction(plateau, "R")
+    assert PlateauMapper.show_probes(plateau) == ["1 2 E"]
+  end
+
+  test "moves drone one space", %{plateau: plateau} do
+    plateau = PlateauMapper.single_instruction(plateau, "M")
+    assert PlateauMapper.show_probes(plateau) == ["1 3 N"]
+  end
+
+  test "invalid instruction", %{plateau: plateau} do
+    plateau = PlateauMapper.single_instruction(plateau, "Z")
+    assert PlateauMapper.show_probes(plateau) == ["1 2 N"]
+    # assert capture_io(plateu) == "invalid instruction `Z`"
+  end
+
+  # move_probe
+  test "moves last probe", %{plateau: plateau} do
+    plateau = PlateauMapper.follow_instructions(plateau, "LMLMLMLMM")
+    assert PlateauMapper.show_probes(plateau) == ["1 3 N"]
   end
 end
